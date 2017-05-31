f3c158be9ad74a229e6b8d153a55b02145f1bad5
Projeto_Oracle <projeto@scsa.corp.com.br>
2017-05-31 18:05:48 -0300

    Initial commit

---
+++ app/src/main/java/br/com/scsa/controledereembolsos/FeedUsuarioActivity.java
@@ -0,0 +1,13 @@
+package br.com.scsa.controledereembolsos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FeedUsuarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed_usuario);
+    }
+}
